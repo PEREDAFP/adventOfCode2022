c05889441bafc922ba3ef573b2e223eb6ce4ec9d
PEREDAFP <PEDROPEREDA@HOTMAIL.COM>
2022-12-02 19:02:31 +0100

    Create day2_2.js

---
+++ day2_2.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require ('fs');
+
+const resultados = new Map ([
+    ['A X', 3],
+    ['A Y', 4],
+    ['A Z', 8],
+    ['B X', 1],
+    ['B Y', 5],
+    ['B Z', 9],
+    ['C X', 2],
+    ['C Y', 6],
+    ['C Z', 7] , 
+]);
+
+const input = fs
+	.readFileSync(path.join('.', 'input.txt'), 'utf8')
+    .toString()
+    .trim()
+  	.split('\n')
+    .reduce((ac,el) => ac += resultados.get(el),0 );
+    		
+console.log(input);
