9c086861b88865b1550ff6551f0b39c374f394e9
PEREDAFP <PEDROPEREDA@HOTMAIL.COM>
2022-12-04 12:16:58 +0100

    Create day4_1.js

---
+++ day4_1.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fs = require ('fs');
+
+const contiene = (lista1, lista2) => 
+            ((lista1[0]<=lista2[0] && lista1[1]>=lista2[1]) || (lista1[0]>=lista2[0] && lista1[1]<=lista2[1]))
+            ? 1 : 0
+    
+const input = fs
+	.readFileSync(path.join('.', 'input.txt'), 'utf8')
+    .trim()
+	.split('\n')
+	.map(el=>
+			el
+			.split(',')
+            .map(el => el
+                    .split('-')
+                    .map(elem => parseInt(elem,10))))
+    .map(el => contiene(el[0], el[1]))
+    .reduce((ac,el)=> ac += el,0)
+    					 
+console.log(input)
